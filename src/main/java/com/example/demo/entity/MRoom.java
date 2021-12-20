3f021b0293120e3ec8c57b0fead83955ffa18a7f
sc30gsw <haidoushi875@gmail.com>
2021-12-21 08:10:27 +0900

    roomsテーブルのエンティティの作成

---
+++ src/main/java/com/example/demo/entity/MRoom.java
@@ -0,0 +1,17 @@
+package com.example.demo.entity;
+
+import java.time.LocalDateTime;
+
+import org.springframework.format.annotation.DateTimeFormat;
+
+import lombok.Data;
+
+@Data
+public class MRoom {
+
+	private int id;
+	private String roomName;
+	
+	@DateTimeFormat(pattern = "yyyy_MM_dd HH:mm:ss")
+	private LocalDateTime createdAt;
+}
