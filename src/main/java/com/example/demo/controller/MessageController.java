3f6b3cf4a3fcdde65e06f773b52452b2a7a9dbb3
sc30gsw <haidoushi875@gmail.com>
2021-12-23 07:32:51 +0900

    チャットルーム一覧の表示

--- src/main/java/com/example/demo/controller/MessageController.java
+++ src/main/java/com/example/demo/controller/MessageController.java
@@ -1,15 +1,34 @@
 package com.example.demo.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.example.demo.entity.MRoom;
+import com.example.demo.service.RoomService;
+import com.example.demo.service.impl.UserDetailServiceImpll;
 
 @Controller
 @RequestMapping("/")
 public class MessageController {
+	
+	@Autowired
+	private RoomService roomService;
 
 	@GetMapping("/")
-	public String getIndex() {
+	public String getIndex(Model model, @AuthenticationPrincipal UserDetailServiceImpll loginUser) {
+		
+		//ログインユーザーのユーザー名取得
+		String username = loginUser.getUser().getName();
+		model.addAttribute("username", username);
+		
+		List<MRoom> rooms =roomService.getLoginUserRooms(loginUser);
+		model.addAttribute("rooms", rooms);
 		
 		return "messages/index";
 	}
