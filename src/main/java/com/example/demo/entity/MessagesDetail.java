616759caa52a05d291c193d74b6276bb518a4607
sc30gsw <haidoushi875@gmail.com>
2022-05-05 16:17:22 +0900

    いいねのカウントを行う処理をするための改修
    Viewでメソッド.size()にて件数を表示する

--- src/main/java/com/example/demo/entity/MessagesDetail.java
+++ src/main/java/com/example/demo/entity/MessagesDetail.java
@@ -1,6 +1,9 @@
 package com.example.demo.entity;
 
 import java.time.LocalDateTime;
+import java.util.List;
+
+import org.springframework.format.annotation.DateTimeFormat;
 
 import lombok.Data;
 
@@ -13,7 +16,17 @@ public class MessagesDetail {
 	private int roomId;
 	private int userId;
 	private String image;
+
+	@DateTimeFormat(pattern = "yyyy_MM_dd HH:mm:ss")
 	private LocalDateTime createdAt;
+
+	@DateTimeFormat(pattern = "yyyy_MM_dd HH:mm:ss")
 	private LocalDateTime updatedAt;
+
+	private List<TLike> likes;
+
+	public List<TLike> likeCounts() {
+		return likes.stream().filter(like -> like.getMessageId() == id).collect(Collectors.toList());
+	}
 	
 }
